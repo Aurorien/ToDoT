38556ceea5bc031cfb187f751a05b697de57e20f
Anna Eklund <anna.eklund@yahoo.se>
2025-08-30 18:55:18 +0200

    Add todo list

---
+++ src/TodoList.tsx
@@ -0,0 +1,43 @@
+import type { ITodo } from "./types";
+import Todo from "./Todo";
+import { useList } from "./hooks/useList.ts";
+import { initialTodos } from "./initialTodos.ts";
+import "./TodoList.css";
+import AddButton from "./AddButton.tsx";
+import { useState } from "react";
+import AddForm from "./AddForm.tsx";
+
+function TodoList() {
+  const todos = useList<ITodo>("todos", initialTodos);
+  const [isAddFormActive, setIsAddFormActive] = useState<boolean>(false);
+
+  if (!todos) {
+    return <div>No todos yet.</div>;
+  }
+
+  const handleOnAdd = (newTodo: ITodo) => {
+    todos.actions.add(newTodo);
+    setIsAddFormActive(false);
+  };
+
+  return (
+    <>
+      <AddButton onClick={() => setIsAddFormActive(true)} />
+      {isAddFormActive && <AddForm onAdd={handleOnAdd} />}
+      <section>
+        <ol className="todo-list">
+          {todos.list.map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              onMove={todos.actions.move}
+              onRemove={todos.actions.remove}
+            />
+          ))}
+        </ol>
+      </section>
+    </>
+  );
+}
+
+export default TodoList;
